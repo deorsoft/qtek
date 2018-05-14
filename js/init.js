6321f63f09814a2745d42ae92b5dc96bf4a13b45
cemeza <39112492+cemeza@users.noreply.github.com>
2018-05-14 12:59:45 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -13,8 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.carousel');
   var instances = M.Carousel.init(elems, options);
   var instance = M.Carousel.init({
-    indicators:true,
-    numVisible :3
+
   });
 });
 
