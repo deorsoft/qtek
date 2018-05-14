6a73fb1c26f448868b5d304db190401928d952cf
cemeza <39112492+cemeza@users.noreply.github.com>
2018-05-14 13:04:28 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -12,7 +12,10 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.carousel');
   var instances = M.Carousel.init(elems, options);
-
+  var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
 });
 
 // Or with jQuery
